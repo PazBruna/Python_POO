477e0cb4252e56bbd25a7b9619aa0a8577966fa0
Bruna Paz <bruna.paz@digisystem.com.br>
2019-02-25 11:35:27 -0300

    Modelo
    Criando classes e objetos.

---
+++ modelo.py
@@ -0,0 +1,61 @@
+class Filme:
+    def __init__(self, nome, ano, duracao): #self é a instancia
+        self.__nome = nome.title() #Colocar __ modifica o objeto p/ private
+        self.ano = ano #public
+        self.duracao = duracao
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self): #Metodo 
+        self.__likes +=1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+class Serie:
+    def __init__(self, nome, ano, temporadas): #self é a instancia
+        self.__nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self): #Metodo 
+        self.__likes +=1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+vingadores = Filme('Vingadores - guerra infinita', 2018, 160) #Objeto
+
+vingadores.dar_like()
+vingadores.dar_like()
+
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano}'
+    f'- Temporadas: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+atlanta = Serie('Atlanta', 2018, 2)
+
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}'
+    f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
