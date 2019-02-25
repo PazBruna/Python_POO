e95322e5be6f698d6f352a4512c555b6fbe1efea
Bruna Paz <bruna.paz@digisystem.com.br>
2019-02-25 15:14:50 -0300

    Polimorfismo
    Utilizando o polimorfismo.

---
+++ polimorfismo.py
@@ -0,0 +1,64 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title() # Apenas UM _ ele apenas protege
+        self.ano = ano
+        self._likes = 0
+        self._recomendado = 0
+        
+    @property
+    def likes (self):
+        return self._likes
+
+    def dar_like (self):
+        self._likes += 1
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def novo_nome(self, novo_nome): #encapsulando o comportamento do nome iniciar com letra mai
+        self._nome = novo_nome.title()
+    
+    @property
+    def recomendado(self):
+        return self._recomendado
+
+    def recomendar(self):
+        self._recomendado += 1
+    
+
+class Filme(Programa): #herdando classe
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano) #Chamando o inicializador da classe "mãe" 
+        self.duracao = duracao
+        
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+        
+   
+vingadores = Filme('Vingadores', 2018, 160)
+pantera = Filme('Pantera', 2018, 95)
+supernatural = Serie('Supernatural', 2018 , 13)
+
+pantera.dar_like()
+supernatural.dar_like()
+supernatural.dar_like()
+supernatural.recomendar()
+vingadores.dar_like()
+pantera.recomendar()
+
+print(f'{vingadores.nome} - {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes} - Recomendado: {vingadores.recomendado} vezes')
+print(f'{pantera.nome} - {pantera.ano} - Duração: {pantera.duracao} - Likes: {pantera.likes} Recomendado: {pantera.recomendado} vezes')
+print(f'{supernatural.nome} - {supernatural.ano} - Duração: {supernatural.temporadas} - Likes: {supernatural.likes} Recomendado: {supernatural.recomendado} vezes')
+
+filmes_series = [vingadores, pantera, supernatural]
+
+for programa in filmes_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas #verifica se o filme ou a serie tem duração senão imprime a temporada
+    print(f'{programa.nome} - {detalhes} -{programa.ano} - Likes: {programa.likes} - Recomendado: {programa.recomendado} vezes')
+    
+
+
