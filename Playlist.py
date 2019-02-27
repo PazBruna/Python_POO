2dc2485312732e5549cea076555a6cc9ea2abf43
PazBruna <brunappaz@outlook.com>
2019-02-27 11:46:04 -0300

    Criando playlist

---
+++ Playlist.py
@@ -0,0 +1,86 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title() # Apenas UM _ ele apenas protege
+        self.ano = ano
+        self._likes = 0
+        self._recomendado = 0
+        
+    @property
+    def likes (self):
+        return self._likes
+
+    def dar_like (self):
+        self._likes += 1
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def novo_nome(self, novo_nome): #encapsulando o comportamento do nome iniciar com letra maiuscula
+        self._nome = novo_nome.title()
+    
+    @property
+    def recomendado(self):
+        return self._recomendado
+
+    def recomendar(self):
+        self._recomendado += 1
+
+    def __str__(self): #Retorna esse dado como string
+        return f'{self._nome} - {self.ano} - Likes: {self._likes} - Recomendado: {self._recomendado} vezes'
+
+class Filme(Programa): #herdando classe
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano) #Chamando o inicializador da classe "mãe" 
+        self.duracao = duracao
+    
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - Duração: {self.duracao}min - Likes: {self._likes} - Recomendado: {self._recomendado} vezes'
+  
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+    
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - Likes: {self._likes} - Recomendado: {self._recomendado} vezes'
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
+
+
+vingadores = Filme('Vingadores', 2018, 160)
+panico = Filme('Todo mundo em panico', 1999, 100)
+pantera = Filme('Pantera', 2018, 95)
+supernatural = Serie('Supernatural', 2018 , 13)
+demolidor = Serie('Demolidor', 2016, 2)
+
+pantera.dar_like()
+supernatural.dar_like()
+supernatural.dar_like()
+supernatural.recomendar()
+supernatural.recomendar()
+supernatural.recomendar()
+vingadores.dar_like()
+pantera.recomendar()
+panico.dar_like()
+panico.recomendar()
+panico.dar_like()
+demolidor.recomendar()
+demolidor.dar_like()
+
+playlist = [vingadores, panico, pantera, supernatural, demolidor]
+playlist_fds = Playlist('Fim de Semana', playlist)
+
+for programa in playlist_fds.programas:
+  print(programa)
+
+print(isinstance(vingadores, Serie)) #False #verificar se o objeto vingadores pertence a classe serie
+
+
