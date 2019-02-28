2a6bee5cae24c6bb899a21c8b229f1fbbfd891b0
PazBruna <brunappaz@outlook.com>
2019-02-28 14:15:04 -0300

    Update herancaMultipla.py

--- herancaMultipla.py
+++ herancaMultipla.py
@@ -1,4 +1,7 @@
 class Funcionario:
+    def __init__(self, nome):
+        self.nome = nome
+    
     def registra_hora(self, horas):
         print('Horas registradas...')
     
@@ -19,19 +22,22 @@ class Alura(Funcionario):
     def busca_perguntas_sem_resposta(self):
         print('Mostrando perguntas não respondidas do forúm')
 
+class Hipster:
+    def __str__(self):
+        return f'Hipster, {self.nome}'
+
 class Junior(Alura):
     pass
 
 class Pleno(Caelum, Alura):
     pass
 
-jose = Junior()
-jose.busca_perguntas_sem_resposta()
-jose.mostrar_tarefas()
-luan = Pleno()
-luan.busca_perguntas_sem_resposta()
-luan.busca_perguntas_sem_resposta()
-luan.mostrar_tarefas()
+class Senior(Alura, Caelum, Hipster):
+    pass
 
 #MRO
 #good head (hierarquia especifica) - Verifica se há outra classe na hierarquia, no caso do pleno, se não houver o metodo na classe Caelum ele irá imprimir o método de Alura.
+
+ricardo = Senior('Ricardo')
+print(ricardo)
+
